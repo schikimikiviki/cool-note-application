b19a824ff8e136482b56bc7897fe46e96687376d
schikimikiviki <viktoria.bzdyk@gmx.at>
2023-10-03 18:44:31 +0200

    editing notes works

--- note-frontend/src/components/NoteList.jsx
+++ note-frontend/src/components/NoteList.jsx
@@ -1,15 +1,69 @@
-const NoteList = ({ notes }) => {
+import React, { useState } from "react";
+import axios from "axios";
+import api from "../api/axiosConfig";
+
+const NoteList = ({ notes, onUpdate }) => {
+  const [editingNote, setEditingNote] = useState(null);
+  const [editedContent, setEditedContent] = useState("");
+
+  const handleEdit = async (noteId) => {
+    try {
+      const noteToEdit = notes.find((note) => note.id === noteId);
+      setEditingNote(noteId);
+      setEditedContent(noteToEdit.content);
+    } catch (error) {
+      console.error("Error while editing note:", error);
+    }
+  };
+
+  const handleSave = async (noteId) => {
+    try {
+      const editedNote = notes.find((note) => note.id === noteId);
+      editedNote.content = editedContent;
+
+      await api.put(`/${noteId}`, editedNote);
+
+      setEditingNote(null);
+
+      onUpdate();
+    } catch (error) {
+      console.error("Error while saving note:", error);
+    }
+  };
+
   return (
     <div className="main-container">
       {notes.map((note) => (
         <div key={note.id} className="note-container relative">
-          <span className="close-button" onClick={() => handleDelete(note.id)}>
-            X
-          </span>
-          <h2 className="handwriting">{note.name}</h2>
-          <div className="handwriting">{note.content}</div>
-          <hr className="line"></hr>
-          <div className="heading-small">Note-id: {note.id}</div>
+          {editingNote === note.id ? (
+            <div>
+              <textarea
+                value={editedContent}
+                onChange={(e) => setEditedContent(e.target.value)}
+              ></textarea>
+              <button onClick={() => handleSave(note.id)}>Save</button>
+            </div>
+          ) : (
+            <div>
+              <span
+                className="close-button"
+                onClick={() => handleDelete(note.id)}
+              >
+                X
+              </span>
+              <h2 className="handwriting">{note.name}</h2>
+              <div className="handwriting">{note.content}</div>
+              <hr className="line"></hr>
+              <div className="heading-small">Note-id: {note.id}</div>
+              <br />
+              <span
+                className="link-default"
+                onClick={() => handleEdit(note.id)}
+              >
+                Edit
+              </span>
+            </div>
+          )}
         </div>
       ))}
     </div>
