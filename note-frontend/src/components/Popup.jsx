9e798b9adea2ef6418113efdc76c9f120a146a2a
schikimikiviki <viktoria.bzdyk@gmx.at>
2024-01-13 21:12:06 +0100

    color selcetion layout added

--- note-frontend/src/components/Popup.jsx
+++ note-frontend/src/components/Popup.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import api from "../api/axiosConfig";
 
 const Popup = ({ onClose, onAdd }) => {
@@ -7,9 +7,23 @@ const Popup = ({ onClose, onAdd }) => {
     content: "",
   });
 
+  const [selectedColor, setSelectedColor] = useState(null);
+
+  const colors = [
+    "#FF595E", // Coral Red
+    "#FFCA3A", // Happy Yellow
+    "#8AC926", // Grass Green
+    "#1982C4", // Sky Blue
+    "#6A4C93", // Grape Violet
+  ];
+
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setNoteData({ ...noteData, [name]: value });
+  };
+
+  const handleColorClick = (color) => {
+    setSelectedColor(color);
   };
 
   const submitForm = async (e) => {
@@ -57,6 +71,35 @@ const Popup = ({ onClose, onAdd }) => {
             value={noteData.content}
             onChange={handleInputChange}
           />
+
+          <div className="colorPicker">
+            <p className="heading-medium" style={{ paddingTop: "1%" }}>
+              Select a color:
+            </p>
+            <div
+              style={{
+                display: "flex",
+                marginLeft: "30px",
+                alignItems: "center",
+              }}
+            >
+              {colors.map((color, index) => (
+                <div
+                  key={index}
+                  onClick={() => handleColorClick(color)}
+                  style={{
+                    width: "50px",
+                    height: "50px",
+                    backgroundColor: color,
+                    marginRight: "10px",
+                    cursor: "pointer",
+                    border: selectedColor === color ? "2px solid #000" : "none",
+                  }}
+                ></div>
+              ))}
+            </div>
+            {/* {selectedColor && <p>Selected Color: {selectedColor}</p>} */}
+          </div>
 
           <button type="submit" className="submit-button">
             Submit
