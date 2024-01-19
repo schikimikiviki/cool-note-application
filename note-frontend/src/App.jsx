082c882d907a9775b542fb6120bafd10159198a3
schikimikiviki <viktoria.bzdyk@gmx.at>
2024-01-19 18:57:40 +0100

    light mode animation

--- note-frontend/src/App.jsx
+++ note-frontend/src/App.jsx
@@ -45,14 +45,37 @@ function App() {
     <div
       className={`main-page ${isDarkThemeSet ? "dark-theme" : "light-theme"}`}
     >
-      {isDarkThemeSet && (
+      {isDarkThemeSet ? (
         <div class="bg-animation">
           <div id="stars"></div>
           <div id="stars2"></div>
           <div id="stars3"></div>
           <div id="stars4"></div>
         </div>
+      ) : (
+        <div id="background-wrap">
+          <div class="x1">
+            <div class="cloud"></div>
+          </div>
+
+          <div class="x2">
+            <div class="cloud"></div>
+          </div>
+
+          <div class="x3">
+            <div class="cloud"></div>
+          </div>
+
+          <div class="x4">
+            <div class="cloud"></div>
+          </div>
+
+          <div class="x5">
+            <div class="cloud"></div>
+          </div>
+        </div>
       )}
+
       <Header onReceive={handleRequest} onClick={handleThemeChange} />
       {isPopupOpen && <Popup onClose={closePopup} onAdd={load} />}
       <NoteList notes={notes} onDelete={handleDeleteFromState} />
