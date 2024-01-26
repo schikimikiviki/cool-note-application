8e1730e0fe4cb58a1b98b8f309d7864b5c06d651
schikimikiviki <viktoria.bzdyk@gmx.at>
2024-01-26 10:32:50 +0100

    about section text and picture

--- note-frontend/src/components/About/About.jsx
+++ note-frontend/src/components/About/About.jsx
@@ -1,4 +1,5 @@
 import "./About.css";
+import picture from "../../assets/about.jpg";
 
 const About = (props) => {
   const onClose = () => {
@@ -13,6 +14,20 @@ const About = (props) => {
         </button>
 
         <h2 className="popup-title">About</h2>
+        <div className="about-container">
+          <div>
+            <img src={picture} width="220" height="280" />
+          </div>
+          <div className="container-text">
+            Hi! My name is Viktoria and this is my Notes Application, where
+            simplicity meets productivity. Organize your thoughts effortlessly
+            with my user-friendly interface. Customize your notes with different
+            colors for easy categorization. Whether you're a student,
+            professional, or creative mind, the Notes App adapts to your needs.
+            Experience the joy of streamlined note-taking, and boost your
+            productivity. Start your journey today and organize your life.
+          </div>
+        </div>
       </div>
     </div>
   );
